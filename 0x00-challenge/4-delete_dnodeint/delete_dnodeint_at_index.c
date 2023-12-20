6be4a9b7159fa61d9cd4b2738f47175ff5748f5a
natnael-shiferaw <natnaelshiferaw2123@gmail.com>
2023-12-20 15:55:32 +0300

    debugging code

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -43,7 +43,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		/*changing (*head)->prev->prev = (*head)->prev; to (*head)->prev->next = (*head)->next;*/
+		/**
+         * Changing (*head)->prev->prev = (*head)->prev;
+         * to (*head)->prev->next = (*head)->next;
+        */
 		(*head)->prev->next = (*head)->next;
 		free(*head);
 		if ((*head)->next)
